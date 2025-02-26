6a354698a38fffdeb1217e17badcbeada7a6a10a
LiterallyMe-kun <sknoah2002@gmail.com>
2025-02-26 13:36:18 -0700

    basic shell models

--- Mission08_Team0115/Models/Task.cs
+++ Mission08_Team0115/Models/Task.cs
@@ -1,7 +1,18 @@
-﻿namespace Mission08_Team0115.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission08_Team0115.Models
 {
     public class Task
     {
-        []
+        [Key]
+        [Required]
+        public int TaskId { get; set; }
+        [Required]
+        public string TaskName { get; set; }
+        public string DueDate { get; set; }
+        [Required]
+        public int Quadrant { get; set; }
+        public int CategoryId  { get; set; }
+        public bool Completed { get; set; }
     }
 }
