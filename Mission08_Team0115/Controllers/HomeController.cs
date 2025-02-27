e7a2838e1d988ba8fa945285f35b413fb776dc0b
aaronwshaw <ashaw32@byu.edu>
2025-02-26 18:59:35 -0700

    Add Temporary stuff to the Controller file. This is very subject to change

--- Mission08_Team0115/Controllers/HomeController.cs
+++ Mission08_Team0115/Controllers/HomeController.cs
@@ -1,17 +1,26 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Mission08_Team0115.Models;
 
 namespace Mission08_Team0115.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        //This is very Temporary
+        private AddEditTask.AddEditTaskContext _context;
+
+        public HomeController(AddEditTask.AddEditTaskContext temp) // constructor
         {
-            _logger = logger;
+            _context = temp;
         }
+
+
+
+
+        //The Queries will definitely maybe be changed to make it work with the database
+
 
         public IActionResult Index()
         {
@@ -20,8 +29,86 @@ namespace Mission08_Team0115.Controllers
 
         public IActionResult Quadrant()
         {
-            return View();
+            var tasks = _context.Tasks
+            .Include(x => x.Category)
+            .OrderBy(x => x.CategoryName).ToList();
+            return View(tasks);
         }
+
+
+        // Takes you to the AddEditTask page to add a new Task
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            ViewBag.Tasks = _context.Tasks
+                .ToList();
+
+            return View("AddEditTask", new Task());
+        }
+
+
+        // Adds the new Task - redirects to confirmation
+        [HttpPost]
+        public IActionResult AddTask(Task response)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Task.Add(response);
+                _context.SaveChanges();
+
+                return View("Confirmation", response);
+            }
+            else
+            {
+                ViewBag.Categories = _context.Categories
+                    .OrderBy(x => x.CategoryName)
+                    .ToList();
+                return View("AddEditTask", response);
+            }
+
+        }
+
+        //Populates the AddEditTask view with the data from the Task you want to edit
+        [HttpGet]
+        public IActionResult Edit(int TaskID)
+        {
+            var task = _context.Tasks
+                .Single(x => x.TaskId == TaskID);
+
+            ViewBag.Categories = _context.Categories.
+                OrderBy(x => x.CategoryName)
+                .ToList();
+
+            return View("AddEditTask", task);
+
+        }
+
+        //Submit the editted Task and redirects to Quadrant
+        [HttpPost]
+        public IActionResult Edit(Task updatedTask)
+        {
+            _context.Tasks.Update(updatedTask);
+            _context.SaveChanges();
+
+            return RedirectToAction("Quadrant");
+        }
+
+
+
+
+
+
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+
 
         public IActionResult Privacy()
         {
